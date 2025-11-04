3dd68bb23fb45679523f840b7ee391475f3b3562
anbassociates <anbassociates.com.np@gmail.com>
2025-11-04 13:23:39 +0545

    Add files via upload

---
+++ js/ss_options.js
@@ -0,0 +1,31 @@
+var ssCountdownDefaultOptions = ssCountdownDefaultOptions || {};
+
+// ssCountdownDefaultOptions.secondsColor 	= 	"#11c8ea";
+// ssCountdownDefaultOptions.minutesColor 	= 	"#ff5153";
+// ssCountdownDefaultOptions.hoursColor 	= 	"#23d0a2";
+// ssCountdownDefaultOptions.daysColor 	= 	"#f9b401";
+
+ssCountdownDefaultOptions.secondsColor  = "#FFFF33"; // Neon Blue (Cyan)
+ssCountdownDefaultOptions.minutesColor  = "#39FF14"; // Neon Red
+ssCountdownDefaultOptions.hoursColor    = "#00FFFF"; // Neon Green
+ssCountdownDefaultOptions.daysColor     = "#FF073A"; // Neon Yellow
+
+
+/*
+ * IMPORTANT NOTES:
+ * The parameters below must be in the correct format, including length of them. 
+ * */
+
+/*Date Format: YYYY-MM-DD*/
+ssCountdownDefaultOptions.date 		= 	"2025-12-24";	
+
+/*Time Format: hh:mm:ss*/
+ssCountdownDefaultOptions.time 		= 	"23:59:59";		
+
+/*Timezone Format: GMT+HH:MM
+ * Example:
+ * GMT+00:00
+ * GMT+08:00
+ * GMT-08:30
+*/
+ssCountdownDefaultOptions.timezone 		= 	"GMT+05:45";
